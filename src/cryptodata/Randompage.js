6e32724f6dfd2c4da467b03a6e998d7fa49caed4
kirti gaurav mishra <kirtigauravmishra@gmail.com>
2022-11-11 00:10:33 +0530

    final deploy

---
+++ src/cryptodata/Randompage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Randompage = () => {
+  return (
+    <div>
+      <h2> Just checking for Random navigating Routes</h2>
+    </div>
+  );
+};
+
+export default Randompage;
